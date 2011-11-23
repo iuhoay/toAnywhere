300460446332eab1acf6cba808011bb09e80f44f
charsky.wu <charsky.wu@gmail.com>
2011-11-23 10:13:25 +0800

    添加注释

--- jquery-toanywhere.js
+++ jquery-toanywhere.js
@@ -1,3 +1,13 @@
+/**
+ * 平滑滚动页面到某元素
+ * <a href="body" id="toBody">to Top</a>
+ * $("#toBody").toAnywhere();
+ * 
+ *
+ * @author wuyaohui
+ * email: charsky.wu@gmail.com
+ * 2011/11/23 
+ */
 (function($) {
 	$.fn.toAnywhere = function() {
 		$(this).click(function() {
